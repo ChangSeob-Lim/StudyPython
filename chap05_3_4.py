0a991d15e086ed3c41b542625c255e9ca5dd3efb
ChangSeob-Lim <limchangsz@gmail.com>
2020-05-14 17:34:19 +0900

    200514 종료

--- chap05_3_4.py
+++ chap05_3_4.py
@@ -1,3 +1,5 @@
+lists = []
+
 with open("./data/result.txt", "r") as f:
     for line in f:
         (name, height, weight) = line.strip().split(", ")
@@ -15,11 +17,21 @@ with open("./data/result.txt", "r") as f:
         else:
             result = "저체중"
 
-        print('\n'.join([
-            "이름 : {}",
-            "키 : {}",
-            "몸무게 : {}",
-            "BMI : {}",
-            "결과 : {}"
-        ]).format(name, height, weight, bmi, result))
-        print()
+        # print('\n'.join([
+        #     "이름 : {}",
+        #     "키 : {}",
+        #     "몸무게 : {}",
+        #     "BMI : {}",
+        #     "결과 : {}"
+        # ]).format(name, height, weight, bmi, result))
+        # print()
+
+        lists.append([name, height, weight, bmi, result])
+
+with open("./data/outputs.txt", "w") as f:
+    # f.write("{}, {}, {}, {}, {}\n".format("이름", "키", "몸무게", "BMI", "결과"))
+
+    for item in lists:        
+        f.write("{}, {}, {}, {}, {}\n".format(item[0],item[1],item[2],item[3],item[4]))
+
+print("파일생성이 완료되었습니다")
